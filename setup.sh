e169d29d15b37a4a4b70b3a9b99aa5359ff234ed
Pugalarasan <75373225+0xPugazh@users.noreply.github.com>
2024-02-05 20:05:39 +0530

    Update setup.sh
    add tools in bin folder
    check whether tools are installed or not

--- setup.sh
+++ setup.sh
@@ -19,10 +19,29 @@ go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 go install -v github.com/tomnomnom/anew@latest
 go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 
+sudo /root/go/bin/subfinder /usr/bin/
+sudo /root/go/bin/amass /usr/bin/
+sudo /root/go/bin/puredns /usr/bin/
+sudo /root/go/bin/naabu /usr/bin/
+sudo /root/go/bin/nuclei /usr/bin/
+sudo /root/go/bin/anew /usr/bin/
+sudo /root/go/bin/notify /usr/bin/
+
 # Install wordlist and resolvers
 cd ~/BugBlaze/
 git clone https://github.com/trickest/resolvers
 wget https://raw.githubusercontent.com/n0kovo/n0kovo_subdomains/main/n0kovo_subdomains_huge.txt
 mv n0kovo_subdomains_huge.txt wordlists.txt
+
+# check if tools are installed
+hash subfinder 2>/dev/null && echo "subfinder: Installed" || echo "subfinder: Not Installed"
+hash amass 2>/dev/null && echo "amass: Installed" || echo "amass: Not Installed"
+hash puredns 2>/dev/null && echo "puredns: Installed" || echo "puredns: Not Installed"
+hash naabu 2>/dev/null && echo "naabu: Installed" || echo "naabu: Not Installed"
+hash nuclei 2>/dev/null && echo "nuclei: Installed" || echo "nuclei: Not Installed"
+hash anew 2>/dev/null && echo "anew: Installed" || echo "anew: Not Installed"
+hash notify 2>/dev/null && echo "notify: Installed" || echo "notify: Not Installed"
+[ -f /root/BugBlaze/wordlists.txt ] && echo "wordlists.txt found" || echo "wordlists.txt not found" 
+[ -f /root/BugBlaze/resolvers/resolvers.txt ] && echo "resolvers.txt found" || echo "resolvers.txt not found"
 
 echo "Done!!!"
